7db985da0160ba7f31d221390121f60821b24d84
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-03-29 15:20:41 +0200

    its green colours

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-#Cv templating on the go
-This project I will be creating of the cv form that can fill out and apply for any position on the go
+# Odin CV Application
+
+This is a simple CV web app that allows you to add your information and display it.
+I built this as part of my learning at the [odin project](https://www.theodinproject.com/lessons/react-new-cv-application).
--- src/components/TitleDate.jsx
+++ src/components/TitleDate.jsx
@@ -1,16 +1,16 @@
 import "../styles/TitleDate.css";
 
 function TitleDate({ children, startDate, endDate }) {
-    startDate = new Date(startDate).toLocaleDateString("en-GB");
-    endDate = new Date(endDate).toLocaleDateString("en-GB");
-    return (
-      <div className="title-date">
-        {children}
-        <p>
-          {startDate} - {endDate}
-        </p>
-      </div>
-    );
-  }
-  
-  export default TitleDate;
+  startDate = new Date(startDate).toLocaleDateString("en-GB");
+  endDate = new Date(endDate).toLocaleDateString("en-GB");
+  return (
+    <div className="title-date">
+      {children}
+      <p>
+        {startDate} - {endDate}
+      </p>
+    </div>
+  );
+}
+
+export default TitleDate;
