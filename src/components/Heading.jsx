939fc46c2160a7e204350068eb463c97d1115edf
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-03-29 14:48:29 +0200

    made the form

---
+++ src/components/Heading.jsx
@@ -0,0 +1,35 @@
+import { FaGithub, FaLinkedin, FaXTwitter, FaTelegram } from "react-icons/fa6";
+import { FiMail } from "react-icons/fi";
+import ExternalLink from "./ExternalLink";
+import LinksList from "./LinksList";
+import Title from "./Title";
+import "../styles/Heading.css";
+
+function Heading({ name, title, email, github, linkedin, twitter }) {
+  return (
+    <div className="heading">
+      <Title name={name} title={title} />
+      <LinksList>
+        <ExternalLink url={`mailto:${email}`}>
+          <FiMail />
+          <span>{email}</span>
+        </ExternalLink>
+        <ExternalLink url={github.url}>
+          <FaGithub />
+          <span>{github.username}</span>
+        </ExternalLink>
+        <ExternalLink url={linkedin.url}>
+          <FaLinkedin />
+          <span>{linkedin.username}</span>
+        </ExternalLink>
+        <ExternalLink url={twitter.url}>
+          <FaXTwitter />
+          <span>{twitter.username}</span>
+        </ExternalLink>
+        
+      </LinksList>
+    </div>
+  );
+}
+
+export default Heading;
---
+++ src/components/Home.jsx
@@ -0,0 +1,49 @@
+import Heading from "./Heading";
+import Card from "./Card";
+
+function isObjectEmpty(obj) {
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) return false;
+  }
+  return true;
+}
+
+function Home({ setPage, handlePrint }) {
+  const allDetails = JSON.parse(localStorage.getItem("allDetails"));
+  const handleClick = () => {
+    setPage("form");
+  };
+  return (
+    <div className="home">
+      <div className="submit-button">
+        <button className="edit-button" onClick={handleClick}>
+          Edit
+        </button>
+        <button onClick={handlePrint}>Print</button>
+      </div>
+      <Heading {...allDetails} />
+      <div>
+        {!isObjectEmpty(allDetails.education) && <h2>Education</h2>}
+        {Object.entries(allDetails.education).map((education) => (
+          <Card key={education[0]} {...education[1]} />
+        ))}
+      </div>
+      <div>
+        {!isObjectEmpty(allDetails.experience) && (
+          <h2>Professional Experience</h2>
+        )}
+        {Object.entries(allDetails.experience).map((experience) => (
+          <Card key={experience[0]} {...experience[1]} />
+        ))}
+      </div>
+      <div>
+        {!isObjectEmpty(allDetails.project) && <h2>Projects</h2>}
+        {Object.entries(allDetails.project).map((project) => (
+          <Card key={project[0]} {...project[1]} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
